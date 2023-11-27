881cf4d010885372cf3535360861bc9b931ba600
Githinji <githinji.ngare@gmail.com>
2023-11-27 14:28:03 +0300

    Modified config to fix db connection

---
+++ app.py
@@ -0,0 +1,8 @@
+from config import tmdb_bp,app
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- config.py
+++ config.py
@@ -5,11 +5,11 @@ app= Flask(__name__)
 tmdb_bp = Blueprint('tmdb', __name__, url_prefix='/tmdb')
 
 TMDB_API_KEY = 'bdafc4661430ae309b0b69639ed9c4ed'
-MONGO_URI = 'mongodb://localhost:27017/'
+# app.config['MONGO_URI'] = 'mongodb://localhost:27017/tmdbdb'
 
+
+client = MongoClient('localhost', 27017)
 DB_NAME = 'tmdb_database'
-client = MongoClient(MONGO_URI)
-
 db = client[DB_NAME]
 
 tmdb_bp = Blueprint('tmdb', __name__, url_prefix='/tmdb')
