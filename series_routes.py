dca626d5b9ba83e89a7734806a7a7416bb85090c
Githinji <githinji.ngare@gmail.com>
2023-11-27 18:00:49 +0300

    implemented try-except for series_routes.py and saved data to MongoDB

--- series_routes.py
+++ series_routes.py
@@ -1,6 +1,6 @@
 #series_routes.py
-from flask import Blueprint
-from config import TMDB_API_KEY
+from flask import Blueprint,jsonify
+from config import TMDB_API_KEY,mongo
 import requests
 
 TMDB_API_URL = 'https://api.themoviedb.org/3/'
@@ -9,10 +9,27 @@ series_bp = Blueprint('series_bp',__name__)
 
 @series_bp.route('/series')
 def fetch_series():
+    try:
+        endpoint = 'tv/top_rated'
+        params = {'api_key': TMDB_API_KEY}
+        response = requests.get(f'{TMDB_API_URL}{endpoint}', params=params)
+        response.raise_for_status
 
-    endpoint = 'tv/top_rated'
-    params = {'api_key': TMDB_API_KEY}
-    response = requests.get(f'{TMDB_API_URL}{endpoint}', params=params)
-    tv_series = response.json()['results']
+        tv_series = response.json()['results']
 
-    return tv_series
+        save_to_db(tv_series)
+
+        return jsonify({'message': 'TV series fetched and saved successfully'})
+
+    except requests.RequestException as e :
+        return jsonify({'success': False, 'message': f'Error fetching movies: {str(e)}'})
+
+
+def save_to_db(tv_series):
+    try:
+        mongo.db.tv_series.delete_many({})
+
+        mongo.db.tv_series.insert_many(tv_series)
+    except Exception as e:
+        
+        return jsonify({ 'message': f'Error saving TV series to MongoDB: {str(e)}'})
