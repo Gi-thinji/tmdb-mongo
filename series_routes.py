a92b8d54fffb0be807460f8ba9dc7b0fc667bc07
Gi-thinji <githinji.ngare@gmail.com>
2024-01-29 15:42:13 +0300

    Modified TMDB endpoint to new endpoint

--- series_routes.py
+++ series_routes.py
@@ -9,29 +9,29 @@ TMDB_API_URL = 'https://api.themoviedb.org/3/'
 
 series_bp = Blueprint('series_bp',__name__)
 
-@series_bp.route('/series')
-def fetch_series():
+@series_bp.route('/series/<int:series_id>')
+def fetch_series(series_id):
     try:
-        endpoint = 'tv/top_rated'
+        endpoint = f'tv/{series_id}'
         params = {'api_key': tmdb_api_key}
         response = requests.get(f'{TMDB_API_URL}{endpoint}', params=params)
         response.raise_for_status()
 
-        tv_series = response.json()['results']
+        tv_show = response.json()
 
-        save_to_db(tv_series)
+        save_to_db(tv_show)
 
         return jsonify({'message': 'TV series fetched and saved successfully'})
 
     except requests.RequestException as e :
-        return jsonify({'success': False, 'message': f'Error fetching movies: {str(e)}'})
+        return jsonify({'success': False, 'message': f'Error fetching movies: {str(e)}'}),500
 
 
-def save_to_db(tv_series):
+def save_to_db(tv_show):
     try:
         mongo.db.tv_series.delete_many({})
 
-        mongo.db.tv_series.insert_many(tv_series)
+        mongo.db.tv_series.insert_one(tv_show)
     except Exception as e:
         
-        return jsonify({ 'message': f'Error saving TV series to MongoDB: {str(e)}'})
+        return jsonify({ 'message': f'Error saving TV series to MongoDB: {str(e)}'}),500
